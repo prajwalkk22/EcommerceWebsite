56663413a84b51999cdb2865884d40a450ce59da
prajwal <prajwal@prajwal.prajwal>
2026-01-23 21:45:35 +0530

    Add Category module with controller, service, and model

---
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -0,0 +1,15 @@
+package com.ecommerce.project.service;
+
+import com.ecommerce.project.model.Category;
+
+import java.util.List;
+
+/**
+ * To  promote loose coupling and modularity it is interface
+ * */
+public interface CategoryService {
+        List<Category> getAllCategories();
+        void createCategory(Category category);
+
+         String deleteCategory(Long categoryId);
+}

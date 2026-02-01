6419510d61bd9e74d0549d00be5c9b7016710282
prajwal <prajwal11kk22@gmail.com>
2026-02-01 13:14:22 +0530

    feat(category): implement sorting for category API

--- src/main/java/com/ecommerce/project/service/CategoryService.java
+++ src/main/java/com/ecommerce/project/service/CategoryService.java
@@ -8,7 +8,7 @@ import com.ecommerce.project.payload.CategoryResponse;
  * To  promote loose coupling and modularity it is interface
  * */
 public interface CategoryService {
-        CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize);
+        CategoryResponse getAllCategories(Integer pageNumber,Integer pageSize,String sortBy,String sortOrder);
         CategoryDTO createCategory(CategoryDTO categoryDTO);
 
         CategoryDTO deleteCategory(Long categoryId);

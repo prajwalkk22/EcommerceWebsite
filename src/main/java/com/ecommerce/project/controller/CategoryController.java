d95ccf796bb1e3773e90654adfdcaca13885b9a9
prajwal <prajwal11kk22@gmail.com>
2026-02-08 08:18:52 +0530

    refactor: add DTO and response layer for product module

--- src/main/java/com/ecommerce/project/controller/CategoryController.java
+++ src/main/java/com/ecommerce/project/controller/CategoryController.java
@@ -23,7 +23,7 @@ public class CategoryController {
             @RequestParam(name = "sortBy",defaultValue = AppConstants.SORT_CATEGORIES_BY,required = false) String sortBy,
             @RequestParam(name = "sortOrder",defaultValue = AppConstants.SORT_DIR,required = false) String sortOrder
             ) {
-        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber,pageSize,sortBy,sortOrder);
+        CategoryResponse categoryResponse = categoryService.getAllCategories(pageNumber,pageSize,sortBy,sortOrder );
         return new ResponseEntity<>(categoryResponse, HttpStatus.OK);
     }
 
---
+++ src/main/java/com/ecommerce/project/service/ProductService.java
@@ -0,0 +1,7 @@
+package com.ecommerce.project.service;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ProductService {
+}
